cd98774c10130b7c5b98f8b7b13c178e3428168a
Isaac Macedo <isaacmacedotr@gmail.com>
2024-08-23 20:15:52 -0300

    refactor: :recycle: Ready for development environment

---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import HomeComponentBlack from '../components/HomeComponentBlack';
+import '../styles/pages/Home.css';
+
+const Home = () => {
+  return (
+    <main className='home'>
+      <HomeComponentBlack />
+    </main>
+  )
+}
+
+export default Home;
