aca17475369f410d238d63b4eae21636395da957
Isaac Macedo <isaacmacedotr@gmail.com>
2024-08-24 14:33:59 -0300

    feat: :sparkles: Start a create - About me

---
+++ front-end/src/components/AboutMeBlack.jsx
@@ -0,0 +1,16 @@
+import '../styles/components/AboutMeBlack.css';
+
+const AboutMeBlack = () => {
+  return (
+    <section id='about' className='section-about-me'>
+      <article className='article-about-me'>
+        <h1 className='about-title'>Sobre mim</h1>
+        <p className='about-des01 gloucester'>
+          I am a full-stack developer, passionate about technology and programming. I am always looking for new challenges and opportunities to learn and improve my skills. I am currently studying systems analysis and development at the Federal Institute of Education, Science and Technology of Ceará (IFCE).
+        </p>
+      </article>
+    </section>
+  );
+}
+
+export default AboutMeBlack;
