5bf86505268b7bbe100d5e04d737c2b0b60b5b2a
Isaac Macedo <isaacmacedotr@gmail.com>
2024-08-24 14:33:38 -0300

    feat: :sparkles: Home page semi-Complete. No image

--- front-end/src/components/HomeComponentBlack.jsx
+++ front-end/src/components/HomeComponentBlack.jsx
@@ -1,9 +1,38 @@
-const homeComponentBlack = () => {
+// import { useEffect } from "react";
+// import TypeIt from "typeit";
+import '../styles/components/HomeComponentBlack.css';
+
+const HomeComponentBlack = () => {
+  /* const animation = () => {
+    new TypeIt("#home-my-name", {
+      speed: 100,
+      waitUntilVisible: true,
+      loop: false,
+    })
+    .type('Isaac Macêdo')
+    .go();
+  };
+  
+  useEffect(() => {
+    animation();
+  }, []); */
+
   return (
-    <div>
-      <h1>Home Component Black</h1>
-    </div>
+    <section className='section-apresentation'>
+      <article className='article-containt-apresentation'>
+        <p id='apresentation-basic' className='gloucester'>Olá eu sou</p>
+        <p id='home-my-name' className='britannic'>Isaac Macêdo</p>
+        <p id='home-title' className='gloucester'>Desenvolvedor <br /> Full-stack</p>
+        <button className="button-contact">
+          <div className="art-button">
+            <p className="text-button-contact britannic">
+              Contato
+            </p>
+          </div>
+        </button>
+      </article>
+    </section>
   );
 }
 
-export default homeComponentBlack;
+export default HomeComponentBlack;
--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import HomeComponentBlack from '../components/HomeComponentBlack';
 import HeaderBlack from '../components/HeaderBlack';
+import AboutMeBlack from '../components/AboutMeBlack';
 import '../styles/pages/Home.css';
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
       <HeaderBlack />
       <div className="line-Yellow-aside"></div>
       <HomeComponentBlack />
+      <AboutMeBlack />
     </main>
   )
 }
