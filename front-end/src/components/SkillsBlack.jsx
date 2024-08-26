b01cd3eded59e10ebc99747f5213e2d85a91db28
Isaac Macedo <isaacmacedotr@gmail.com>
2024-08-26 10:01:31 -0300

    feat: :sparkles: Start the Skill page and alteration in HeaderBlack.css

---
+++ front-end/src/components/SkillsBlack.jsx
@@ -0,0 +1,17 @@
+import '../styles/components/SkillsBlack.css';
+
+const SkillsBlack = () => {
+  return (
+    <section className='section-skills'>
+      <main className='article-skills'>
+        <div className='skills-title'>
+          <div className='skills-bg-purple-left' />
+          <h2 className='gloucester'>Skills</h2>
+          <div className='skills-bg-purple-right' />
+        </div>
+      </main>
+    </section>
+  )
+}
+
+export default SkillsBlack;
