334a83a9d8ae23d948afeeb3e93110ba863c78f2
Isaac Macedo <isaacmacedotr@gmail.com>
2024-08-26 10:01:07 -0300

    feat: :sparkles: Alteration in HomeComponent and Home.css

--- front-end/src/components/HomeComponentBlack.jsx
+++ front-end/src/components/HomeComponentBlack.jsx
@@ -4,13 +4,17 @@ import '../styles/components/HomeComponentBlack.css';
 
 const HomeComponentBlack = () => {
   /* const animation = () => {
+    const name = ['Isaac Macêdo']
     new TypeIt("#home-my-name", {
-      speed: 100,
+      speed: 200,
       waitUntilVisible: true,
-      loop: false,
+      loop: true,
     })
-    .type('Isaac Macêdo')
-    .go();
+    .pause(500)
+    .type(name[0])
+    .pause(2000)
+    .delete(name[0].length)
+    .go()
   };
   
   useEffect(() => {
@@ -20,16 +24,19 @@ const HomeComponentBlack = () => {
   return (
     <section className='section-apresentation'>
       <article className='article-containt-apresentation'>
-        <p id='apresentation-basic' className='gloucester'>Olá eu sou</p>
-        <p id='home-my-name' className='britannic'>Isaac Macêdo</p>
-        <p id='home-title' className='gloucester'>Desenvolvedor <br /> Full-stack</p>
-        <button className="button-contact">
-          <div className="art-button">
-            <p className="text-button-contact britannic">
-              Contato
-            </p>
-          </div>
-        </button>
+        <div className="home-text-apresentation">
+          <p id='apresentation-basic' className='gloucester'>Olá eu sou</p>
+          <p id='home-my-name' className='britannic'>Isaac Macêdo</p>
+          <p id='home-title' className='gloucester'>Desenvolvedor <br /> Full-stack</p>
+          <button className="button-contact">
+            <div className="art-button">
+              <p className="text-button-contact britannic">
+                Contato
+              </p>
+            </div>
+          </button>
+        </div>
+        <div className="home-bg-purple"></div>
       </article>
     </section>
   );
